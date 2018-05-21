b7faabc779d30cba0cf74a36d6aac82c6cd97d0f
Happy Situ <happy.situ@clutchanalytics.com>
2018-05-20 20:22:20 -0500

    component skeleton

---
+++ react-client/src/components/Component1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Xxxxx = (props) => (
+  <div>
+
+  </div>
+);
+
+export default Xxxxx;
---
+++ react-client/src/components/Component2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Xxx = (props) => (
+  <div>
+
+  </div>
+);
+
+export default Xxx;
