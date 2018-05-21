6586521180a1a773f985450279d522e1d83aa9f1
Happy Situ <happy.situ@clutchanalytics.com>
2018-05-20 20:19:37 -0500

    more skeleton

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,2 +1,27 @@
 import React from 'react';
-import App from './App.jsx';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+import Component1 from './components/Component1.jsx';
+import Component2 from './components/Component2.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  //write functions
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
