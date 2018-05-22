9d72214ff7d058e99fd7243837ef5b9293428866
Ted Green <ted.green29@gmail.com>
2018-05-22 01:04:32 -0700

    start initial ticketmaster queries

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const db = require('../database');
+const tm = require('../tm/tm.js');
 
 const app = express();
 const PORT = 3000;
@@ -15,6 +16,26 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+
+});
+
+app.get('/ticketmaster', (req, res) => {
+  var testDate1 = new Date('30 May 2018 00:00 UTC').toISOString().split('.')[0]+'Z';
+  var testDate2 = new Date('30 October 2018 00:00 UTC').toISOString().split('.')[0]+'Z';
+
+  console.log(testDate1);
+  console.log(testDate2);
+
+  var test = {
+    city: 'San Francisco',
+    startDate: testDate1,
+    endDate: testDate2,
+    stateCode: 'CA',
+    size: 10
+  };
+
+  tm(test, (stringified) => res.end(JSON.stringify(stringified)));
+
 
 });
 
