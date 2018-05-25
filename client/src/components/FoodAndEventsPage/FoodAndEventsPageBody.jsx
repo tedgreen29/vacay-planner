ecfce1b0fb4557eae7b670d8d4b6693dad7d7b7c
Debalina <mukherjee.debalina@gmail.com>
2018-05-25 13:11:47 -0700

    (feat) make FoodAndEventsPageBody a stateful component and add function to get restaurant by location

--- client/src/components/FoodAndEventsPage/FoodAndEventsPageBody.jsx
+++ client/src/components/FoodAndEventsPage/FoodAndEventsPageBody.jsx
@@ -2,14 +2,55 @@ import React from 'react';
 import FoodTabContent from './FoodTabContent.jsx';
 import EventsTabContent from './EventsTabContent.jsx';
 import { Tab } from 'semantic-ui-react';
+import $ from 'jquery';
 
-const panes = [
-  { menuItem: 'Food', render: () => <Tab.Pane><FoodTabContent /></Tab.Pane> },
-  { menuItem: 'Events', render: () => <Tab.Pane><EventsTabContent /></Tab.Pane> }
-]
 
-const FoodAndEventsPageBody = () => (
-  <Tab panes={panes} />
-)
+class FoodAndEventsPageBody extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurantList: [],
+      location: 'San Francisco'
+    };
+  }
+
+  componentDidMount() {
+    this.getRestaurantsByLocaton(this.state.location);
+  }
+
+  getRestaurantsByLocaton(location) {
+    console.log('test!!')
+    $.ajax({
+      type: 'GET',
+      url: `/restaurants/${location}`,
+      success: result => {
+        console.log('result', result);
+        this.setState({
+          restaurantList: result.businesses
+        }
+      );    
+      }
+    });
+  }
+
+
+  render() {
+    const panes = [
+      { menuItem: 'Food', render: () => 
+        <Tab.Pane>
+          <FoodTabContent 
+            restaurantList = {this.state.restaurantList}
+          />
+        </Tab.Pane> },
+      { menuItem: 'Events', render: () => <Tab.Pane><EventsTabContent /></Tab.Pane> }
+    ]
+    return (
+      <div>
+        <Tab panes={panes} />
+      </div>
+    );
+  }
+}
 
 export default FoodAndEventsPageBody
