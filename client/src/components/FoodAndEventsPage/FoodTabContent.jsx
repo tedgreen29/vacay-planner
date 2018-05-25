e0bd8aa1791531d350c20ba76e17477b6ae172b2
Debalina <mukherjee.debalina@gmail.com>
2018-05-25 13:12:42 -0700

    (feat) display restaurant list - first cut

--- client/src/components/FoodAndEventsPage/FoodTabContent.jsx
+++ client/src/components/FoodAndEventsPage/FoodTabContent.jsx
@@ -1,65 +1,36 @@
 import React from 'react'
 import { Button, Card, Image, Icon } from 'semantic-ui-react'
 
-const FoodTabContent = () => (
+const FoodTabContent = (props) => (
   <Card.Group>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src='/media/mapAndAirplaneCoffee.jpg' />
-        <Card.Header>
-          Restaurant 1
-        </Card.Header>
-        <Card.Meta>
-          Rating 1
-        </Card.Meta>
-        <Card.Description>
-          Description 1 <strong>Category 1</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='red'><Icon name="empty heart" /></Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src='/media/mapAndAirplaneCoffee.jpg' />
-        <Card.Header>
-          Restaurant 2
-        </Card.Header>
-        <Card.Meta>
-          Rating 2
-        </Card.Meta>
-        <Card.Description>
-          Description 2 <strong>Category 2</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='red'><Icon name="empty heart" /></Button>
-        </div>
-      </Card.Content>
-    </Card>
-    <Card>
-      <Card.Content>
-        <Image floated='right' size='mini' src='/media/mapAndAirplaneCoffee.jpg' />
-        <Card.Header>
-          Restaurant 3
-        </Card.Header>
-        <Card.Meta>
-          Rating 3
-        </Card.Meta>
-        <Card.Description>
-          Description 3 xxxxxxxx xxxxxxxx xxxxxxxx <strong>Category 3</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='red'><Icon name="empty heart" /></Button>
-        </div>
-      </Card.Content>
-    </Card>
+    {props.restaurantList.map((restaurant) => {
+      return (
+      <Card fluid>
+        <Card.Content>
+          <Image floated='right' size='small' src={restaurant.image_url} />
+          <Card.Header>
+            { restaurant.name }
+          </Card.Header>
+          <Card.Meta>
+          Rating: { restaurant.rating } Stars
+          </Card.Meta>
+          <Card.Description>
+            {restaurant.display_address}
+          </Card.Description>
+          <Card.Description>
+            {restaurant.categories.map(category => {
+                return category.title
+            }).join(', ')} 
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='red'><Icon name="empty heart" /></Button>
+          </div>
+        </Card.Content>
+      </Card>
+      )
+    })}
   </Card.Group>
 )
 
