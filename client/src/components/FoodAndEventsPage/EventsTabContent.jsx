d6127da38658ea4aec30ee438a83ecd843bf0fdb
Debalina <mukherjee.debalina@gmail.com>
2018-05-27 11:04:43 -0700

    (feat) display event date

--- client/src/components/FoodAndEventsPage/EventsTabContent.jsx
+++ client/src/components/FoodAndEventsPage/EventsTabContent.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Card, Image, Icon, Item, Segment } from 'semantic-ui-react'
+import moment from 'moment'
+import { Button, Card, Image, Icon, Item, Segment, Label } from 'semantic-ui-react'
 
 const EventsTabContent = (props) => (
   <Segment.Group>
@@ -26,6 +27,13 @@ const EventsTabContent = (props) => (
                   ${event._embedded.venues[0].postalCode}` 
                 } 
               </Item.Description>
+              <Item.Extra>
+                <Label 
+                  style={ {textTransform: 'uppercase', backgroundColor: '#00aced', color: 'white'} }
+                >
+                  {moment(event.dates.start.dateTime).format('MMM DD ddd')}
+                </Label>
+              </Item.Extra>
             </Item.Content>
           </Item>
         </Item.Group>
