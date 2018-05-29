bffb7694a95c81ed6a8608186eb4e921dbe15702
Debalina <mukherjee.debalina@gmail.com>
2018-05-29 01:24:47 -0700

    (feat) mytrips - render tabs with hardcoded data

--- client/src/components/MyTripsPage/MyTripsPageBody.jsx
+++ client/src/components/MyTripsPage/MyTripsPageBody.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Card, Icon, Grid } from 'semantic-ui-react';
+import { Tab } from 'semantic-ui-react';
 
-const MyTripsPageBody = (props) => (
-  <div>
-    My Trips!
-  </div> 
-);
+const panes = [
+{ menuItem: 'Trip 1', render: () => <Tab.Pane>Content 1</Tab.Pane> },
+{ menuItem: 'Trip 2', render: () => <Tab.Pane>Content 2</Tab.Pane> },
+{ menuItem: 'Trip 3', render: () => <Tab.Pane>Content 3</Tab.Pane> },
+]
+
+
+const MyTripsPageBody = () => 
+  <Tab 
+    menu={{ fluid: true, vertical: true, tabular: true }} 
+    panes={panes} 
+  />
+ 
+
 
 export default MyTripsPageBody;
