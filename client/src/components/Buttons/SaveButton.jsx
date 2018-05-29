f222173ff53aea50a3244ff06f85d1d7a5b800cb
Debalina <mukherjee.debalina@gmail.com>
2018-05-28 21:52:49 -0700

    add onClick method to toggle heart icon

--- client/src/components/Buttons/SaveButton.jsx
+++ client/src/components/Buttons/SaveButton.jsx
@@ -3,23 +3,32 @@ import { Button, Icon } from 'semantic-ui-react';
 
 class SaveButton extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-          isSaved: false
-      }
+    super(props);
+    this.state = {
+      isSaved: false
+    }
+    this.onClick = this.onClick.bind(this);
   }
 
   onClick(e) {
-      this.setState({
-        isSaved: !this.state.isSaved
-      });
+    this.setState({
+      isSaved: !this.state.isSaved
+    });
   }
 
   render() {
     return (
-      <Button icon floated='right' basic color='red' style={ {marginRight: 20} }>
-        <Icon style={ {color: 'red'} } name="empty heart" />
-      </Button>
+      <div>
+        { this.state.isSaved ?
+        (<Button icon floated='right' basic color='red' style={ {marginRight: 20} }  onClick={this.onClick}>
+          <Icon style={ {color: 'red'} } name="heart" />
+        </Button>) 
+        :
+        (<Button icon floated='right' basic color='red' style={ {marginRight: 20} }  onClick={this.onClick}>
+          <Icon style={ {color: 'red'} } name="empty heart" />
+        </Button>
+        )}
+      </div>
     );
   }
 }
