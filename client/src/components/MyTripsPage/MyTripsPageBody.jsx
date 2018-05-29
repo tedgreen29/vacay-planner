48e084edccc5074113bb5f756355b7d4f4b4f2df
Debalina <mukherjee.debalina@gmail.com>
2018-05-29 00:46:45 -0700

    (feat) starting on my trips page

---
+++ client/src/components/MyTripsPage/MyTripsPageBody.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card, Icon, Grid } from 'semantic-ui-react';
+
+const MyTripsPageBody = (props) => (
+  <div>
+    My Trips!
+  </div> 
+);
+
+export default MyTripsPageBody;
