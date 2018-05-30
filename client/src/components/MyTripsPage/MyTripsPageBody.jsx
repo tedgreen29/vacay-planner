438205deac9c86f5043f32b5bea7d97bc750627a
Debalina <mukherjee.debalina@gmail.com>
2018-05-30 02:51:55 -0700

    (feat) add method to get trip details

--- client/src/components/MyTripsPage/MyTripsPageBody.jsx
+++ client/src/components/MyTripsPage/MyTripsPageBody.jsx
@@ -2,20 +2,42 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import { Menu } from 'semantic-ui-react'
 import SelectTrip from './SelectTrip.jsx';
+import $ from 'jquery';
 
 class MyTripsPageBody extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTrip: '1'
+      selectedTrip: '1',
+      eventsSelected: [],
+      restaurantsSelected: []
     }
     this.updateSelection = this.updateSelection.bind(this);
   }
-
+  
+  componentDidMount() {
+    this.updateSelection(this.state.selectedTrip);
+  }
+  
   updateSelection(tripId) {
     this.setState(() => {
       return {
         selectedTrip: tripId
+      }
+    })
+    this.getTripDetails(tripId);
+  }
+
+  getTripDetails(tripId) {
+    $.ajax({
+      type: 'GET',
+      url: '/trips',
+      success: result => {
+        console.log(result)
+        this.setState({
+          eventsSelected: JSON.parse(result)[0].events,
+          restaurantsSelected: JSON.parse(result)[0].restaurants
+        })
       }
     })
   }
