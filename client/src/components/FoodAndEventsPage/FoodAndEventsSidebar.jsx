f6064742c677ccc4fb6779680f04d6b009716bad
Happy Situ <happy.situ@clutchanalytics.com>
2018-05-29 23:58:23 -0500

    make sidebar into menu to match style

--- client/src/components/FoodAndEventsPage/FoodAndEventsSidebar.jsx
+++ client/src/components/FoodAndEventsPage/FoodAndEventsSidebar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Button, Card, Image, Icon, Input, Segment } from 'semantic-ui-react';
+import { Button, Card, Image, Icon, Input, Segment, Tab } from 'semantic-ui-react';
 
-const FoodAndEventsSidebar = () => (
-    <Segment>
-        <Input focus placeholder="Enter your trip name here" /><br />
-
-        
-    </Segment>
-)
+const FoodAndEventsSidebar = () => {
+    const panes = [
+        { menuItem: 'Favorites', render: () => (
+            <Tab.Pane>
+                <Input focus placeholder="Enter your trip name here" /><br />
+            </Tab.Pane> 
+            )
+        }
+    ];
+    return <Tab panes={panes} />;
+}
 
 export default FoodAndEventsSidebar;
