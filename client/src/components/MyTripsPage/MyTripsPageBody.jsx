3e02d8de24613d7a452b5f5099a955ece3f2efe4
Debalina <mukherjee.debalina@gmail.com>
2018-05-30 23:38:08 -0700

    (refactor) refactor myTrips page to render saved events and Restaurants in 2 separate sections

--- client/src/components/MyTripsPage/MyTripsPageBody.jsx
+++ client/src/components/MyTripsPage/MyTripsPageBody.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import moment from 'moment';
-import { Grid } from 'semantic-ui-react';
+import { Grid,  Accordion, Icon } from 'semantic-ui-react';
 import SelectTrip from './SelectTrip.jsx';
 import EventsList from './EventsList.jsx';
 import $ from 'jquery';
@@ -19,6 +19,14 @@ class MyTripsPageBody extends React.Component {
   
   componentDidMount() {
     this.updateSelection(this.state.selectedTrip);
+  }
+
+  handleClick(e, titleProps) {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
   }
 
   updateSelection(tripId) {
@@ -45,17 +53,35 @@ class MyTripsPageBody extends React.Component {
   }
 
   render() {
+    const { activeIndex } = this.state
     return (
       <div> 
-        <Grid columns='equal' style={ {marginTop: 10, backgroundColor: 'white'} }>
-          <Grid.Column left='true' width={3}>
+        <Grid columns='equal' style={ { marginTop: 50, backgroundColor: 'white'} }>
+          <Grid.Column floated='left' width={3}>
             <SelectTrip
               selectedTrip = {this.state.selectedTrip}
               onSelect = {this.updateSelection}
             />
           </Grid.Column>
-          <Grid.Column width={13}>
+          <Grid.Column floated='right' width={13}>
+            <Accordion fluid styled>
+              <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick.bind(this)}>
+                <Icon name='dropdown' />
+                Saved Events
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 0}>
+                <p> </p>
             {!this.state.eventsSelected ? <p>Loading....</p> : <EventsList eventsSelected={this.state.eventsSelected}/>}
+              </Accordion.Content>
+              <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick.bind(this)}>
+                <Icon name='dropdown' />
+                Saved Restaurants
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 1}>
+                <p> </p>
+                Restaurant List
+              </Accordion.Content>
+            </Accordion>
           </Grid.Column>
         </Grid>
       </div>
