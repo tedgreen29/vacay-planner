20796e443d204c32061d2b0f82bcbbacfbbd9477
Debalina <mukherjee.debalina@gmail.com>
2018-06-01 03:13:22 -0700

    (refactor) refactor sidebar to render fav items list

--- client/src/components/FoodAndEventsPage/FoodAndEventsSidebar.jsx
+++ client/src/components/FoodAndEventsPage/FoodAndEventsSidebar.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import { Button, Card, Image, Icon, Input, Segment, Tab, Item, Divider } from 'semantic-ui-react';
+import { Button, Card, Image, Icon, Input, Segment, Tab, Item, Divider, List } from 'semantic-ui-react';
 
 import FavFoodCard from './FavFoodCard.jsx';
 import FavEventCard from './FavEventCard.jsx';
 
 const FoodAndEventsSidebar = (props) => {
-    const panes = [
-        {
-            menuItem: 'Favorites', render: () => (
-                <Tab.Pane>
-                    <Input focus placeholder="Enter your trip name here" value={props.tripName} onChange={props.onNameChange} /><br /><br />
-                    {props.foodFavorites.length > 0 ? <h2>Restaurants</h2> : ""}
-                    <Card.Group>
-                        {props.foodFavorites.map((restaurant, index) => {
-                            return <FavFoodCard restaurant={restaurant} key={"favfood" + restaurant.id} />;
-                        })}
-                    </Card.Group>
-                    {props.eventFavorites.length > 0 ? <h2>Events</h2> : ""}
-                    <Card.Group>
-                        {props.eventFavorites.map((event, index) => {
-                            return <FavEventCard event={event} key={"favevent" + event.id} />;
-                        })}
-                    </Card.Group>
-                    {props.foodFavorites.length + props.eventFavorites.length === 0 ? "" : (
-                        <div>
-                            <Divider />
-                            <Button onClick={props.saveTrip}>Save to My Trips</Button>
-                        </div>
-                    )}
-                </Tab.Pane>
-            )
-        }
-    ];
-    return <Tab panes={panes} />;
+  const panes = [
+    {
+      menuItem: 'Favorites', render: () => (
+        <Tab.Pane>
+          <Input focus placeholder="Enter your trip name here" value={props.tripName} onChange={props.onNameChange} /><br /><br />
+          {props.foodFavorites.length > 0 ? <h2>Restaurants</h2> : ''}
+          <List celled size='large'>
+            {props.foodFavorites.map((restaurant, index) => {
+              return <FavFoodCard restaurant={restaurant} key={'favfood' + restaurant.id} />;
+            })}
+          </List>
+          {props.eventFavorites.length > 0 ? <h2>Events</h2> : ''}
+          <List celled size='large'>
+            {props.eventFavorites.map((event, index) => {
+                return <FavEventCard event={event} key={'favevent' + event.id} />;
+            })}
+          </List>
+          {props.foodFavorites.length + props.eventFavorites.length === 0 ? '' : (
+            <div>
+              <Button onClick={props.saveTrip}>Save to My Trips</Button>
+            </div>
+          )}
+        </Tab.Pane>
+      )
+    }
+  ];
+  return <Tab panes={panes} />;
 }
 
 export default FoodAndEventsSidebar;
+
