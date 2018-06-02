a3be8ea8ab066e7da68bf57ec6a39ad3c6b26364
Debalina <mukherjee.debalina@gmail.com>
2018-06-02 00:41:26 -0700

    (feat) display events sorted by date

--- client/src/components/FoodAndEventsPage/EventsTabContent.jsx
+++ client/src/components/FoodAndEventsPage/EventsTabContent.jsx
@@ -5,46 +5,49 @@ import { Button, Card, Image, Icon, Item, Segment, Label } from 'semantic-ui-rea
 
 const EventsTabContent = (props) => (
   <div>
-    {props.eventsList.map((event, index) => {
-      return (
-        <Card fluid key={event.id}>
-          <Item.Group>
-            <Item>
-              <Item.Image className='event-image'
-                size='small'
-                src={event.images[0].url}
-                style={{ margin: 15 }}
-              />
-              <Item.Content>
-                <Item.Header style={{ marginTop: 20 }} className='event-name'>
-                  {`${index + 1}. ${event.name}`}
-                </Item.Header>
-                <Item.Description>
-                  <strong>{event._embedded.venues[0].name}</strong>
-                  {`,                    
-                  ${event._embedded.venues[0].address.line1}, 
-                  ${event._embedded.venues[0].city.name},
-                  ${event._embedded.venues[0].state.stateCode}  
-                  ${event._embedded.venues[0].postalCode}`
-                  }
-                </Item.Description>
-                <Item.Extra>
-                  <Label
-                    style={{ textTransform: 'uppercase', backgroundColor: '#00aced', color: 'white' }}
-                  >
-                    {moment(event.dates.start.dateTime).format('MMM DD ddd')}
-                  </Label>
-                  < SaveButton
-                    toggleFavorite={() => props.toggleFavorite(index, 'events')}
-                    isSaved={props.eventFavorites.find(eventfav => eventfav.id === event.id) ? true : false}
-                  />
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Card>
-      )
-    })}
+    {props.eventsList
+      .sort((a, b) => (a.dates.start.dateTime) - (b.dates.start.dateTime))
+      .map((event, index) => {
+        return (
+          <Card fluid key={event.id}>
+            <Item.Group>
+              <Item>
+                <Item.Image className='event-image'
+                  size='small'
+                  src={event.images[0].url}
+                  style={{ margin: 15 }}
+                />
+                <Item.Content>
+                  <Item.Header style={{ marginTop: 20 }} className='event-name'>
+                    {`${index + 1}. ${event.name}`}
+                  </Item.Header>
+                  <Item.Description>
+                    <strong>{event._embedded.venues[0].name}</strong>
+                    {`,                    
+                    ${event._embedded.venues[0].address.line1}, 
+                    ${event._embedded.venues[0].city.name},
+                    ${event._embedded.venues[0].state.stateCode}  
+                    ${event._embedded.venues[0].postalCode}`
+                    }
+                  </Item.Description>
+                  <Item.Extra>
+                    <Label
+                      style={{ textTransform: 'uppercase', backgroundColor: '#00aced', color: 'white' }}
+                    >
+                      {moment(event.dates.start.dateTime).format('MMM DD ddd')}
+                    </Label>
+                    < SaveButton
+                      toggleFavorite={() => props.toggleFavorite(index, 'events')}
+                      isSaved={props.eventFavorites.find(eventfav => eventfav.id === event.id) ? true : false}
+                    />
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Card>
+        )
+      })
+    }
   </div>
 )
 
--- client/src/components/MyTripsPage/EventsList.jsx
+++ client/src/components/MyTripsPage/EventsList.jsx
@@ -5,36 +5,39 @@ import { Menu, Image, Item, Header, Card, Button, Label } from 'semantic-ui-reac
 function EventsList(props) {
   return (
     <div style={{marginTop: -25}}>
-      {props.eventsSelected.map((event, index) => {
-        return (
-          <Card fluid key={event.id}>
-            <Item.Group>
-              <Item>
-                <Item.Image className='event-image'
-                  size='small'
-                  src={ event.eventImg }
-                  style={ {margin: 15} }
-                />
-                <Item.Content>
-                  <Item.Header style={{marginTop: 20}} className='event-name'>
-                    {`${index + 1}. ${event.name}`}
-                  </Item.Header>
-                  <Item.Description>
-                    <strong>{event.venueName}, </strong>{event.venueAddress}
-                  </Item.Description>
-                  <Item.Extra>
-                    <Label
-                      style={ {textTransform: 'uppercase', backgroundColor: '#D3D3D3', color: '#2A2A2A'} }
-                    >
-                      {moment(event.start_date).format('MMM DD ddd')}
-                    </Label>
-                  </Item.Extra>
-                </Item.Content>
-              </Item>
-            </Item.Group>
-          </Card>
-        )
-      })}
+      {props.eventsSelected
+        .sort((a, b) => (a.dates.start.dateTime) - (b.dates.start.dateTime))
+        .map((event, index) => {
+          return (
+            <Card fluid key={event.id}>
+              <Item.Group>
+                <Item>
+                  <Item.Image className='event-image'
+                    size='small'
+                    src={ event.eventImg }
+                    style={ {margin: 15} }
+                  />
+                  <Item.Content>
+                    <Item.Header style={{marginTop: 20}} className='event-name'>
+                      {`${index + 1}. ${event.name}`}
+                    </Item.Header>
+                    <Item.Description>
+                      <strong>{event.venueName}, </strong>{event.venueAddress}
+                    </Item.Description>
+                    <Item.Extra>
+                      <Label
+                        style={ {textTransform: 'uppercase', backgroundColor: '#D3D3D3', color: '#2A2A2A'} }
+                      >
+                        {moment(event.start_date).format('MMM DD ddd')}
+                      </Label>
+                    </Item.Extra>
+                  </Item.Content>
+                </Item>
+              </Item.Group>
+            </Card>
+          )
+        })
+      }
     </div>
   )
 }
